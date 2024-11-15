71f81f89a69ab48b7ae5b0773482486ecbe11b70
maitri.trivedi <trivedimaitri1@gmail.com>
2024-11-15 22:11:53 +0530

    Started Mini Project

---
+++ ESD_MINI/src/main/java/com/maitri/esd_mini/EsdMiniApplication.java
@@ -0,0 +1,13 @@
+package com.maitri.esd_mini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsdMiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsdMiniApplication.class, args);
+    }
+
+}
